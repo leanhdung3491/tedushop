afbb2c5f462064161d5e115138489b594ef0d630
Don Lee <leanhdung3491@gmail.com>
2017-12-29 18:03:52 +0700

    Update Models Dec 29

---
+++ TeduShop.Model/Abstract/ProductCategory.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Abstract
+{
+    [Table("ProductCategories")]
+    public class ProductCategory : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]//id auto increase
+        public int ID { set; get; }
+        [Required]
+        public string Name { set; get; }
+        [Required]
+        public string Alias { set; get; }
+        public string Description { set; get; }
+        public int? ParentID { set; get; }
+        public int? DisplayOrder { set; get; }
+        public string Image { set; get; }
+        public bool? HomeFlag { set; get; }
+    }
+}
