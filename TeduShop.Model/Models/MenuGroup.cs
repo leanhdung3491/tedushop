fddebcb78726acb209c4bc8abf26e867b6d5309d
Don Lee <leanhdung3491@gmail.com>
2018-01-02 20:44:19 +0700

    Add some file 2044Jan02

--- TeduShop.Model/Models/MenuGroup.cs
+++ TeduShop.Model/Models/MenuGroup.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -8,9 +9,11 @@ namespace TeduShop.Model.Models
     public class MenuGroup
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { set; get; }
 
         [Required]
+        [MaxLength(50)]
         public string Name { set; get; }
 
         public virtual IEnumerable<Menu> Menus { set; get; }
